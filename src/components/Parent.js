6229a9bd8bd510fe3536046556c8e0390738f21f
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-09-21 07:52:39 +0000

    k

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,23 +1,45 @@
-import React  , {useState} from "react";
-import Child from "./Child";
+import React, { useState } from "react";
 
 const Parent = () => {
-  
-    const[ isLoggedIn , setIsLoggedIn] = useState(false)
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return(
+	const submitBtnHandler = (e) => {
+		// Prevent Default:
+		e.preventDefault();
 
-        <div>
-            <h1>Parent Component</h1>
-          {
-         (!isLoggedIn) &&  <Child setIsLoggedIn = {setIsLoggedIn} />
-          }
-          {
-            (isLoggedIn) && <p>You are logged in!</p>
-          }
-        </div>
-    )
-}
+		// Getting the values from the input boxs:
+		const userName = e.target.username.value;
+		const password = e.target.password.value;
 
+		// // Checking for Empty Values:
+		// if (!(userName && password)) {
+		// 	return;
+		// }
 
-export  default Parent
+		setIsLoggedIn((prev) => !prev);
+
+		// Reset the form after processing the values (optional)
+		e.target.reset();
+	};
+	return (
+		<>
+			<h1>Parent Component</h1>
+			{!isLoggedIn && (
+				<form onSubmit={submitBtnHandler}>
+					<div>
+						<label htmlFor='username'>Username:</label>
+						<input type='text' name='username' />
+					</div>
+					<div>
+						<label htmlFor='password'>Password:</label>
+						<input type='password' name='password' />
+					</div>
+					<button>Login</button>
+				</form>
+			)}
+			{isLoggedIn && <p>You are logged in!</p>}
+		</>
+	);
+};
+
+export default Parent;
