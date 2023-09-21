a865ba6ebecfc88f52f998991271d6f06dd762ae
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-09-21 08:03:09 +0000

    j

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from "react";
-import "./../styles/App.css";
-import Parent from "./Parent";
+
+import React,{useState} from "react";
+import './../styles/App.css';
 
 const App = () => {
-	return (
-		<div>
-			{/* Do not remove the main div */}
-			<Parent />
-		</div>
-	);
-};
+  const[isLoggedInd,setIsLoggedIn]=useState(false);
 
-export default App;
+  function SubmitFn(){
+    setIsLoggedIn(true);
+  }
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        {!isLoggedInd&&<form onSubmit={SubmitFn}>
+          <label htmlFor="nameInput">Username:</label>
+          <input type="text" name="nameInput"/><br />
+          <label htmlFor="passwordInput">Password: </label>
+          <input type="password" name="passwordInput"/>
+          <br />
+          <button type="submit">Login</button>
+        </form>}
+        {
+          isLoggedInd&&<p>You are logged in!</p>
+        }
+    </div>
+  )
+}
+
+export default App
